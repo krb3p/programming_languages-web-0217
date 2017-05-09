2aa0c442fa63619a50575cd03b7385a2bd36bb92
krb3p <ryankatherineburke@gmail.com>
2017-05-09 15:22:08 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  language_attributes = {}
+  languages.each do |style, language|
+    language.each do |language, attribute_hash|
+      attribute_hash.each do |attribute, value|
+        if language_attributes[language].nil?
+          language_attributes[language] = {}
+        end
+        language_attributes[language][:style] ||= []
+        language_attributes[language][:style] << style
+        language_attributes[language][attribute] = value
+      end
+    end
+  end
+  language_attributes
 end
